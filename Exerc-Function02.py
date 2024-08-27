ea884a8b877f220278ac68c26524b6be2869fd75
Pedrin0o <pedralecuke@gmail.com>
2024-08-27 14:16:34 -0300

    Update Exerc-Function02.py

--- Exerc-Function02.py
+++ Exerc-Function02.py
@@ -1,6 +1,11 @@
-aula = "Programação Back End"
+def reajustar_salario(salario):
+    if salario > 5000:
+        reajuste = salario * 0.10
+    else:
+        reajuste = salario * 0.15
+    salario_reajustado = salario + reajuste
+    return salario_reajustado
 
-def mostraAula(disciplina):
-    print(f"Disciplina de programação: {disciplina}")
-
-mostraAula(aula)
+salario_atual = float(input("Digite o salário atual: R$"))
+novo_salario = reajustar_salario(salario_atual)
+print(f"O novo salário é: R${novo_salario:.2f}")
